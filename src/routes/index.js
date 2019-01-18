e5ef5a3db6043fd7f2a45b49ff5d30ca3852ae48
Abhishek <abhishek.guruswamy@estuate.com>
2019-01-18 15:03:19 +0530

    code split

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,24 @@
-import Desert from '../pages/desert';
-import Forest from '../pages/forest';
-import CommonPage from '../pages';
+import React from 'react'
+
+// import Desert from '../pages/desert';
+// import Forest from '../pages/forest';
+// import CommonPage from '../pages';
+import Loadable from 'react-loadable';
+
+const Desert = Loadable({
+    loader: () => (import('../pages/desert')),
+    loading: () => <div>Loading...</div>,
+});
+
+const Forest = Loadable({
+    loader: () => (import('../pages/forest')),
+    loading: () => <div>Loading...</div>,
+});
+
+const CommonPage = Loadable({
+    loader: () => (import('../pages')),
+    loading: () => <div>Loading...</div>,
+})
 
 export const routes = [
     {
